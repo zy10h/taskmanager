ee3725926e64596a5c3f31d929bdd3e0484be76d
Ziyang <zy10h97@gmail.com>
2025-08-14 02:38:10 +1000

    changed in testing and workflows 3

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://3.27.158.47:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
